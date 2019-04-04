11e92ffa0a2503e282d25300077da56f8a82f231
Jake <jacobstefan92@gmail.com>
2019-04-04 14:20:01 -0700

    Updates

---
+++ precache-manifest.f4e61f9f840bfdeee7540ba0d9907cdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f12864a4fc01a53bbf1",
+    "url": "/quote_generator/static/js/runtime~main.45866308.js"
+  },
+  {
+    "revision": "2b72662a5256633e128d",
+    "url": "/quote_generator/static/js/main.786130cf.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/quote_generator/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2b72662a5256633e128d",
+    "url": "/quote_generator/static/css/main.69b11442.chunk.css"
+  },
+  {
+    "revision": "8edb6bf9b4cae6fd54377a5ea5652b78",
+    "url": "/quote_generator/index.html"
+  }
+];
